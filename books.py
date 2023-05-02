56aba28770954d2175c3dce3d1f8fa89d5f10d0a
ranerlich-johnbrice <ranerlich@gmail.com>
2023-05-02 21:00:59 +0300

    chat gpt added

--- books.py
+++ books.py
@@ -11,6 +11,7 @@ class Book:
         self.author = author
         self.year_published = year_published
         self.book_type = book_type
+        self.gpt = "my_gpt"
 
     def print(self):  # better use __str__
         print(
